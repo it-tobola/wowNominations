e77d92de61c4e9b00ee33c88eaf91f65f4e26f2f
wherestomi <oawodiya1@gmail.com>
2023-03-08 11:43:52 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -7,12 +7,12 @@ notion_token = "secret_omL8nzIdOZySUeAtSOCHm0bUNh2ydXdohuePKPBXkxm"
 # employee df
 ee_db_id = "03764697bdf74f2b938313815cf62069"
 ee_url = "https://www.notion.so/03764697bdf74f2b938313815cf62069?v=e856d446c1a44cfcb8857b014f591284"
-ee_df = pd.DataFrame(nd.download(ee_db_id, api_key=notion_token, resolve_relation_values=True))
+ee_df = pd.DataFrame(nd.download(ee_db_id, api_key=notion_token))
 
 # wow award df
 wow_db_id = "96c47cd2ff3941829267a40b6223b5c9"
 wow_url = "https://www.notion.so/96c47cd2ff3941829267a40b6223b5c9?v=36f63746938544a0b941d14f94b2ec21&pvs=4"
-wow_df = pd.DataFrame(nd.download(wow_db_id, api_key=notion_token, resolve_relation_values=True))
+wow_df = pd.DataFrame(nd.download(wow_db_id, api_key=notion_token))
 
 # Create a dataframe of active employees for nomination
 active_ee = ee_df[["EE Code", "Full Name", "Home Base"]][ee_df["Status"] == "Active"].reset_index()
